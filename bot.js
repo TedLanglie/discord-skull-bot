7a5b861e1cf98b4462aac8945a08b52ea22de18a
TedLanglie <tedwardlanglie@gmail.com>
2022-07-09 13:44:03 -0700

    Added link to embed, added testing for previous message to avoid spam, removed memories and graveyard as readable channels

--- bot.js
+++ bot.js
@@ -7,19 +7,29 @@ client.on('ready', () => {
     console.log("Bot is ready to go!")
 })
 
-client.on('messageReactionAdd', (reaction, user) => {
-    let limit = 5; // number of skull reactions you need
-    if (reaction.emoji.name == '💀' && reaction.count == limit)
-    {
-        let graveEmbed = new Discord.MessageEmbed()
-        .setColor('#c35748')
-        .setAuthor({name: reaction.message.author.username})
-        .setTitle('"' + reaction.message.content + '"')
-        .setImage(reaction.message.author.displayAvatarURL())
-        .setThumbnail('https://images.emojiterra.com/twitter/512px/1f480.png');
-    
-        client.channels.cache.get('995253766007828571').send({ embeds: [graveEmbed] });
+let previousMessage = '';
 
+client.on('messageReactionAdd', (reaction, user) => {
+    let limit = 6; // number of skull reactions you need
+
+    if(reaction.message.channel.id !== "995253766007828571" && reaction.message.channel.id !== "893205918836211762")
+    {
+        if (reaction.emoji.name == '💀' && reaction.count == limit)
+        {
+            if(reaction.message.content !== previousMessage)
+            {
+                let graveEmbed = new Discord.MessageEmbed()
+                .setColor('#c35748')
+                .setAuthor({name: reaction.message.author.username})
+                .setTitle('"' + reaction.message.content + '"')
+                .setImage(reaction.message.author.displayAvatarURL())
+                .setDescription(reaction.message.url)
+                .setThumbnail('https://images.emojiterra.com/twitter/512px/1f480.png');
+
+                previousMessage = reaction.message.content;
+                client.channels.cache.get('995253766007828571').send({ embeds: [graveEmbed] });
+            }
+        }
     }
   });
 
